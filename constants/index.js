0ce75b9d3330237f1436ed372a6726c38a5ed486
AloneeN3 <140120876+AloneeN3@users.noreply.github.com>
2023-07-21 00:21:05 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xce0cb08cfd5f99c429400021b34e99947b20e028";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
